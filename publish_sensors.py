60ec26a93f953c11104566f2eb0f157620d0a030
selimmch <selim.mch@gmail.com>
2020-12-01 17:06:58 +0100

    capteur-publish2

--- publish_sensors.py
+++ publish_sensors.py
@@ -1,12 +1,16 @@
 import sys
+import time
 import numpy as np
 from capteur.capteur import capteur
 import paho.mqtt.publish as publish
 
 
 def main(args):
-	temp  = capteur(0.6).get_temp()
-	publish.single("capteur/temp", str(temp), hostname="localhost")
+	while(1):
+		temp  = capteur(0.7).get_temp()
+		publish.single("capteur/temp", str(temp), hostname="localhost")
+		time.sleep(.100)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
