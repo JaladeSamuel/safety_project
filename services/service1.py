6ea01554b6cb7bb0bf205eb6a50a14a569fe79a6
Aurelien Montmejat <aurelien.montmejat@gmail.com>
2020-12-01 17:03:36 +0100

    Abonnement MQTT du service 1 au capteur

--- services/service1.py
+++ services/service1.py
@@ -1,5 +1,26 @@
+import paho.mqtt.client as mqtt
+
+print("Initialisation du service 1")
+client = mqtt.Client("service1")
+client.connect("localhost")
+client.subscribe("/sensors/temp")
+client.on_message = on_message
+
+buffer = []
+seuil = 30
+
+def on_message(client, userdata, message):
+    if message.topic == "/sensors/temp":
+        if len(buffer) > seuil:
+            traitement(buffer)
+            buffer = []
+        
+        buffer.append(message.payload.decode("utf-8"))
+    pass
+
+def traitement(donnees):
+    # thomas à toi de jouer
+    pass
+
 while True:
-    # lire les donnees capteurs
-    # les traiter
-    # voir si on nous a demande si on est reveille
     pass
