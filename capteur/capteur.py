a7c401ac45451911946f4bf9ecdf99466bddedd8
JaladeSamuel <sam34440@hotmail.fr>
2021-01-24 22:45:07 +0100

    Fixing indent

--- capteur/capteur.py
+++ capteur/capteur.py
@@ -10,21 +10,23 @@ class capteur :
 
 	def get_temp(self):
 		tmp = random.random_sample()
-        if (tmp+self.failrate < 1):
-            res = 19.5 + random.random_sample()
-        else:
-            res = -1
-        return res 
+		if (tmp+self.failrate < 1):
+			res = 19.5 + random.random_sample()
+		else:
+			res = -1
+		return res 
+
 	def get_failrate(self):
 		return self.failrate
 
 	def publish(self):
 		while(1):
 			publish.single("capteur/temp", str(self.get_temp()), hostname="localhost")
+			print("Sending " + str(self.get_temp()))
 			time.sleep(.100)
 		
 def main(args):
-	print(capteur(.9).publish())
+	print(capteur(.1).publish())
 
 if __name__ == '__main__':
     main(sys.argv[1:])
